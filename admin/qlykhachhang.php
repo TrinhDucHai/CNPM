4af5317e8b2e0e449afc535e2ec3f450339319e2
TrinhDucHai <trinhduchai17@gmail.com>
2022-03-23 21:39:32 +0700

    sprint 2 TDH update 2

--- admin/qlykhachhang.php
+++ admin/qlykhachhang.php
@@ -85,13 +85,7 @@
       <?php 
       include "footer.php";
      ?>
-  <?php 
- include "ControlSidebar.php";
-?>
-      <!-- Control Sidebar -->
   
-      <!-- Add the sidebar's background. This div must be placed
-           immediately after the control sidebar -->
       <div class="control-sidebar-bg"></div>
     </div><!-- ./wrapper -->
 
